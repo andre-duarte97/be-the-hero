6fa65af4222b847e25189b33166fcf67950a6112
andre_97 <andre@realizati.com.br>
2020-04-13 02:04:13 -0300

    back-end e front-end do app

---
+++ backend/src/controllers/ongController.js
@@ -0,0 +1,51 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+
+module.exports = {
+
+    async index(request, response) {        
+        const ongs = await connection('ongs').select('*');
+
+        return response.json(ongs);
+
+    },
+
+    async create(request, response) {
+        const {name, email, whatsapp, city, uf} = request.body;
+        
+        const id = crypto.randomBytes(4).toString('HEX');
+    
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+    
+        return response.json( {id} );
+    },
+
+    async delete(request, response) {
+        const {id} = request.params
+        
+        const ong_id = await connection('ongs').select('id')
+            .where('id', id)
+            .first();
+        
+
+        if (ong_id === undefined  || ong_id.id != id) {
+            return response.status(401).json( { error: ' Operation not permitted.'});
+        }
+
+        await connection('ongs').delete()
+            .where('id', id)
+        
+        return response.status(204).send();          
+
+    }
+
+
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const ongController = require('./controllers/ongController')
+const incidentController = require('./controllers/incidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+routes.delete('/ongs/:id', ongController.delete)
+
+routes.get('/profile', ProfileController.index)
+
+routes.get('/incidents', incidentController.index)
+routes.post('/incidents', incidentController.create)
+routes.delete('/incidents/:id', incidentController.delete)
+
+
+module.exports = routes;
